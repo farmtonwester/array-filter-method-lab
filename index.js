fabfce1b412ca74bb402f287ea7154a3e0644154
Weston Farmer <wesfarmer11@gmail.com>
2024-01-10 16:16:42 -0600

    tests pass, 3 functions complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arrayOfDrivers, string) {
+    const newArray = arrayOfDrivers.filter((element) => element.toLowerCase() === string.toLowerCase())
+    return newArray
+}
+
+function fuzzyMatch(arrayOfDrivers, string) {
+    const newArray = arrayOfDrivers.filter((element) => element[0] === string[0])
+    return newArray
+}
+
+function matchName (arrayOfDriverObjects, string) {
+    const newArray = arrayOfDriverObjects.filter((element) => element.name === string)
+        return newArray
+}
